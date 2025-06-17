9743ab18288affa5c810fff99941c78d9bb128a5
junha0729 <junha0729@dgsw.hs.kr>
2025-06-17 20:32:54 +0900

    서버

--- 백준.py
+++ 백준.py
@@ -1,16 +1,18 @@
-n,m,k = map(int,input().split())
-ans1 = []
-ans2 = []
-cnt = 0
-for i in range(m):
-    ans1.append(True)
-for i in range(n-m):
-    ans1.append(False)
-for i in range(k):
-    ans2.append(True)
-for i in range(n-k):
-    ans2.append(False)
-for i in range(n):
-    if ans1[i] == ans2[i]:
-        cnt += 1
-print(cnt)
+import sys
+input = sys.stdin.readline
+a,b = map(int,input().split())
+cnt1 = []
+cnt2 = []
+ans = []
+for i in range(a):
+    cnt1.append(input())
+for ii in range(b):
+    cnt2.append(input())
+for i in cnt1:
+    for ii in cnt2:
+        if i == ii:
+            ans.append(i)
+ans.sort()
+print(len(ans))
+for i in ans:
+    print(i)
