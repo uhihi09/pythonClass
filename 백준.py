20d14b6490a056b7004ef9819db30dda92dfed12
junha0729 <junha0729@dgsw.hs.kr>
2025-08-26 19:39:33 +0900

    feat: 로그인 기능 개발 완료

--- 백준.py
+++ 백준.py
@@ -1,9 +1,2 @@
-a = int(input())
-for i in range(a):
-    b = list(input().split())
-    cnt = b[0]
-    b[1] = int(b[1])
-    b[2] = int(b[2])
-    cnt = list(cnt)
-    del cnt[b[1]:b[2]]
-    print("".join(map(str, cnt)))
+for i in range(10000):
+    print(-1)
--- 수업.py
+++ 수업.py
@@ -1,21 +1,6 @@
-def gcd(a,b):
-    if b==0:
-        return a
-    else:
-        return gcd(b,a%b)
-ans = []
-cnt = []
-ans1 = []
-while True:
-    try:
-        a = list(map(int, input().split()))
-        ans.append(a)
-    except EOFError:
-        for i in range(len(ans)):
-            for ii in range(len(ans[i])):
-                cnt.append(ans[i][ii])
-        for i in range(len(cnt)):
-            for ii in range(i+1,len(cnt)):
-                ans1.append(gcd(cnt[i],cnt[ii]))
-        print(max(ans1))
-        break
+a = list(input())
+b = list(input())
+ans = ""
+for i in range(len(a)):
+    ans += max(a[i], b[i])
+print(ans)
